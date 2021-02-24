6698a3f1b8f46b66adbf2eb29298c92309b8a302
walterbaya <walterbaya@yahoo.com>
2021-02-24 15:15:47 -0300

    se sube hasta la seccion de selectores en jquery

---
+++ JQuery/dom.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+//Accediendo a los elementos Padres
+	/*$('#tercera').parent().css({
+		background: '#1b3d82'
+		});
+		*/
+//Accediendo a los parents los padres todos...
+//	$('#tercera').parents();
+
+//Accediendo a los elementos hijos
+	//$('#padre').children().fadeOut(1000);
+	//$('#padre').children('#tercera').fadeOut(1000);
+
+//Find, permite buscar dentro de todos los niveles hijos posibles.
+	//$('#contenedor').find('div.caja').slideUp();
+
+//Siblings, permite seleccionar elementos hermanos.
+	//$('#tercera').siblings().fadeOut(1500);
+
+//Next - Prev, acceder a elementos siguientes y anteriores.
+/*
+	$('#tercera').prev().css({
+		background: '#000'
+	});
+
+	$('#tercera').next().css({
+		background: '#000'
+	});
+
+
+	$('#tercera').prevAll().css({
+		background: '#000'
+	});
+
+	$('#tercera').nextAll().css({
+		background: '#000'
+	});
+*/
+
+});
---
+++ JQuery/efectos.js
@@ -0,0 +1,12 @@
+//$(document).ready(function(){
+	function efecto(){
+		//$('.caja').hide(3000, function(){alert("oculto");});
+		//$('.caja').hide(show);
+		//$('.caja').toggle(1000);
+		//$('.caja').fadeIn(1000);
+		//$('.caja').fadeOut(1000);
+		//$('.caja').fadeToggle(1000);
+		//$('.caja').slideUp(1000);
+		//$('.caja').slideDown(1000);
+	}
+//})
