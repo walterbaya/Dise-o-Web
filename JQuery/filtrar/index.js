2f02fb6aa92a78f527268f3c9ffded6339aa6520
walterbaya <walterbaya@yahoo.com>
2021-03-10 16:11:53 -0300

    se suben modos de filtrado de elementos

---
+++ JQuery/filtrar/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	//Obtener la primer caja y aplicarle
+	//estilo css
+	//$('.caja').first().css({background : '#000'})	
+	
+	//Obtener la ultima caja y aplicarle
+	//estilo css
+	//$('.caja').last().css({background : '#000'})
+
+	//Obtener la i+1 esima caja y aplicarle
+	//estilo css
+	//$('.caja').eq(2).css({background: '#456'})
+
+	//el filtro permite seleccionar y obtener muchas cajas a la vez
+	//en cambio con eq solo podemos seleccionar una de manera vectorial
+	//$('.caja').filter('#segunda').css({background: '#fff'})
+
+	//Seleccionar todas las cajas menos las que tienen id segunda
+	//$('.caja').not('#segunda').css({background: 'red'})
+}
+)
